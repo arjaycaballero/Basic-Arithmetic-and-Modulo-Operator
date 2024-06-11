1d7abf7ac751384bfc13c25c090a3534e1a26643
Arjay Caballero <130941463+arjaycaballero@users.noreply.github.com>
2024-06-11 14:19:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Addition var a = 1 + 1;
+// Subtraction var b = 1 - 1;
+// Multiplication var c = 1 * 1;
+// Division var d = 1 / 1;
+// Modulo var e = 12 % 8; answer is 4 the remainder. 
+
+var dogAge= prompt("How old would your dog be if it was a human?");
+
+var humanAge = (dogAge - 2) * 4 + 21;
+alert(humanAge + " is the age of your dog in human age");
